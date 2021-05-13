7539e02f04dbe4e037341c8d0d619c021c5a9542
moochan <hippopo.web@gmail.com>
2021-05-13 23:52:14 +0900

    feat: lesson3

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const ul = document.getElementById('ul');
-const li = document.createElement('li');
-const a = document.createElement('a');
-a.href = '1.html';
-const img = document.createElement('img');
-img.src = 'bookmark.png';
-img.alt = 'ブックマーク';
-a.textContent = 'これです';
-// メモ：insertBeforeでも動くけどprependが記述量減って良い！
-a.insertBefore(img, a.firstChild);
-// a.prepend(img);※IE非対応
-li.appendChild(a);
-ul.appendChild(li);
+
+// これは連想配列？
+const listData = {
+  data1: {
+    href:'1.html',
+    src:'/img/bookmark.png',
+    text:'a1'
+  },
+  data2: {
+    href:'2.html',
+    src:'/img/bookmark.png',
+    text:'a2'
+  }
+}
+
+Object.keys(listData).forEach((key)=>{
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  a.textContent = listData[key].text;
+  a.href = listData[key].href;
+  const img = document.createElement('img');
+  img.src = listData[key].src;
+  a.insertBefore(img, a.firstChild);
+  li.appendChild(a);
+  ul.appendChild(li);
+})
