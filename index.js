c5f292591f522862f3fd7f072cdc39f09fa93321
hippopo-moo <hippopo.web@gmail.com>
2021-05-16 09:54:38 +0900

    lesson4 first commit

--- index.js
+++ index.js
@@ -1,28 +1,19 @@
 const ul = document.getElementById('ul');
 
-// これは連想配列？→連想配列！
-const listData = {
-  data1: {
-    href:'1.html',
-    src:'/img/bookmark.png',
-    text:'a1'
-  },
-  data2: {
-    href:'2.html',
-    src:'/img/bookmark.png',
-    text:'a2'
-  }
-}
+const listData = [
+  {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
+  {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
+]
 
-Object.values(listData).forEach((data)=>{
+listData.forEach(data => {
   const li = document.createElement('li');
   const a = document.createElement('a');
   a.textContent = data.text;
-  a.href = data.href;
+  a.href = data.to;
   const img = document.createElement('img');
-  img.src = data.src;
+  img.src = data.img;
+  img.alt = data.alt;
   a.insertBefore(img, a.firstChild);
   li.appendChild(a);
   ul.appendChild(li);
-})
-
+});
