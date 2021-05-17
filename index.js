c7f2c5c47959a2c8f721e2061d4f22bb2a4457bf
hippopo-moo <hippopo.web@gmail.com>
2021-05-17 22:15:33 +0900

    fix: lesson4 fb

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const ul = document.getElementById('ul');
-
+const fragment = document.createDocumentFragment();
 const listData = [
   {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
@@ -13,7 +13,7 @@ listData.forEach(data => {
   const img = document.createElement('img');
   img.src = data.img;
   img.alt = data.alt;
-  a.insertBefore(img, a.firstChild);
-  li.appendChild(a);
-  ul.appendChild(li);
+  fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);  
 });
+console.log(fragment.childNodes);
+ul.appendChild(fragment);
