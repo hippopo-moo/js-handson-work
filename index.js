2e402d026af979a7205b01744cec9a52b08d8e4d
hippopo-moo <hippopo.web@gmail.com>
2021-06-01 23:39:36 +0900

    feat: lesson9

--- index.js
+++ index.js
@@ -6,23 +6,28 @@ const listData = [
 ]
 const loadingImage = document.getElementById("loading");
 
-const getListData = ()=>{
-  return new Promise(function(resolve, reject){
-    try {
-      setTimeout(() => {
-        reject('error')
-      }, 3000);
-    } catch (error) {
-    }
-  })
+const getListData = async ()=>{
+  try {
+    return listData;
+  } catch (error) {
+    return 'error';
+  }
 }
 
-const createList = ()=> {
-  getListData()
-  .then(()=>{
-  })
-  .catch((error)=>{
-    console.error(error);
-  })
+const createList = async ()=> {
+  const data = await getListData();
+  console.log(data);
+  loadingImage.style.display = "none";
+  listData.forEach(data => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.textContent = data.text;
+    a.href = data.to;
+    const img = document.createElement('img');
+    img.src = data.img;
+    img.alt = data.alt;
+    fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);  
+  });
+  ul.appendChild(fragment);
 }
 createList();
