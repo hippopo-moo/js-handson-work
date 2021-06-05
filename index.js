974f81ccb31426c33ded93c80c3b21e0c4862d75
hippopo-moo <hippopo.web@gmail.com>
2021-06-06 00:37:38 +0900

    fix: lesson9 fb

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
+const main = document.getElementsByTagName('main')[0];
 const ul = document.getElementById('ul');
 const fragment = document.createDocumentFragment();
-const listData = [
+const fragmentLoadingImage = document.createDocumentFragment();
+const div = document.createElement('div');
+div.id = "loading"
+const img = document.createElement('img');
+img.src = "./loading-circle.gif";
+fragmentLoadingImage.appendChild(div).appendChild(img)
+
+const list = [
   {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
 ]
-const loadingImage = document.getElementById("loading");
+// const loadingImage = document.getElementById("loading");
 
-const getListData = async ()=>{
-  try {
-    return listData;
-  } catch (error) {
-    return 'error';
-  }
+const getListData = () => {
+  return new Promise((resolve)=> {
+    try {
+      setTimeout(() => {
+        resolve(list);
+      }, 3000);
+    } catch (error) {
+      console.error(error);
+    }
+  });
 }
 
 const createList = async ()=> {
-  const data = await getListData();
-  console.log(data);
-  loadingImage.style.display = "none";
-  listData.forEach(data => {
+  main.appendChild(fragmentLoadingImage);
+  const listData = await getListData();
+  listData.forEach( listDataItem => {
     const li = document.createElement('li');
     const a = document.createElement('a');
-    a.textContent = data.text;
-    a.href = data.to;
+    a.textContent = listDataItem.text;
+    a.href = listDataItem.to;
     const img = document.createElement('img');
-    img.src = data.img;
-    img.alt = data.alt;
+    img.src = listDataItem.img;
+    img.alt = listDataItem.alt;
     fragment.appendChild(li).appendChild(a).insertBefore(img, a.firstChild);  
   });
+  main.removeChild(document.getElementById('loading'));
   ul.appendChild(fragment);
 }
 createList();
