a135b2c0e84bf780744486144d1a38dc88565040
hippopo-moo <hippopo.web@gmail.com>
2021-06-12 20:15:39 +0900

    fix: lesson10 fb

--- lesson10/index.js
+++ lesson10/index.js
@@ -1,17 +1,25 @@
 const main = document.getElementsByTagName('main')[0];
 const ul = document.getElementById('ul');
 const fragment = document.createDocumentFragment();
-const fragmentLoadingImage = document.createDocumentFragment();
-const div = document.createElement('div');
-div.id = "loading"
-const img = document.createElement('img');
-img.src = "./loading-circle.gif";
-fragmentLoadingImage.appendChild(div).appendChild(img)
 
 const list = [
   {to: "bookmark.html", img: "1.png", alt:"画像1", text: "ブックマーク"},
   {to: "message.html", img: "2.png", alt:"画像2", text: "メッセージ"}
 ]
+
+const createLoadingImage = () => {
+  const fragmentLoadingImage = document.createDocumentFragment();
+  const div = document.createElement('div');
+  div.id = "loading"
+  const img = document.createElement('img');
+  img.src = "./loading-circle.gif";
+  fragmentLoadingImage.appendChild(div).appendChild(img)
+  main.appendChild(fragmentLoadingImage);
+}
+
+const removeLoadingImage = () => {
+  main.removeChild(document.getElementById('loading'));
+}
 
 const getListData = () => {
   return new Promise((resolve)=> {
@@ -28,9 +36,9 @@ const getListData = () => {
 }
 
 const createList = async ()=> {
-  main.appendChild(fragmentLoadingImage);
+  createLoadingImage();
   const listData = await getListData();
-  main.removeChild(document.getElementById('loading'));
+  removeLoadingImage();
   listData.forEach( listDataItem => {
     const li = document.createElement('li');
     const a = document.createElement('a');
