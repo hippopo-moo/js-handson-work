5f27df35755ca4354c6fb3fdc96f5e05d5b7d62d
hippopo-moo <hippopo.web@gmail.com>
2021-06-26 21:59:19 +0900

    fix: 不要な定数定義を削除

--- lesson13/index.js
+++ lesson13/index.js
@@ -4,7 +4,6 @@ const fragment = document.createDocumentFragment();
 const url = 'https://jsondata.okiba.me/v1/json/xL3LX210624191849';
 const fetchBtn = document.getElementById("js-btn-fetch"); 
 const modal = document.getElementById("js-modal");
-const modalContent = document.getElementById("js-modal-content");
 const modalBody = document.getElementById("js-modal-body");
 const modalOverlay = document.getElementById("js-modal-overlay");
 
