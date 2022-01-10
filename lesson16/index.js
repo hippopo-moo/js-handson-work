7c8a70e7b1d4b2cde4439fba4fee07cbb9da4395
hippopo-moo <hippopo.web@gmail.com>
2022-01-10 21:45:32 +0900

    feat: Get articles and images.

--- lesson16/index.js
+++ lesson16/index.js
@@ -1,6 +1,7 @@
 // import { v4 as uuidv4 } from 'uuid';
 // const url = 'https://jsondata.okiba.me/v1/json/cp3sC210927201738';
 const url = "test.json";
+let allData = {};
 const newsBlock = document.querySelector('.news');
 const newsTabs = document.querySelector('.news_Tabs');
 const newsContentWrapper = document.querySelector('.news_ContentWrapper');
@@ -30,7 +31,7 @@ const createNewsContent = (allData)=> {
   primaryElement.appendChild(lists);
   primaryElement.appendChild(primaryImageWrapper);
   newsBlock.appendChild(primaryElement);
-  // console.log(allData);
+  console.log(allData);
   allData.forEach((content, index)=>{
     if(content.isActive){
       primaryImageWrapper.classList.add("news_Img");
@@ -57,25 +58,103 @@ const createNewsContent = (allData)=> {
     // event.targetにis-activeをつける
     // event.targetのidを取得し、それに紐づく記事と画像を取得
     const target = event.target;
+    const tabsItems = newsTabs.querySelectorAll("li");
+    console.log(allData)
     if(target.textContent === 'news'){
       event.target.classList.add('is-active');
-      console.log(typeof(newsTabs[0]));
-      console.log(newsTabs[0]);
-      console.log(newsTabs[1]);
-      console.log(newsTabs[2]);
-      newsTabs[1].classList.remove('is-active');
-      newsTabs[2].classList.remove('is-active');
+      tabsItems[1].classList.remove('is-active');
+      tabsItems[2].classList.remove('is-active');
+      console.log(allData[0])
+      const newsList = document.querySelector('.news_Lists');
+      console.log(newsList)
+
+      const imgWrapper = document.querySelector('.news_Img');
+      imgWrapper.innerHTML = "";
+      const img = document.createElement('img');
+      img.src = allData[0].image.img;
+      img.alt = allData[0].image.alt;
+      imgWrapper.appendChild(img);
+      console.log(img);
+
+      newsList.innerHTML = "";
+      Object.keys(allData[0].article).forEach((key)=>{
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const comment = document.createElement('span');
+        a.href = allData[0].article[key].url;
+        a.textContent = allData[0].article[key].title;
+        comment.textContent = allData[0].article[key].commentCount;
+        li.appendChild(a);
+        li.appendChild(comment);
+        lists.appendChild(li);
+        if(allData[0].article[key].is_new){
+          console.log('new article');
+        }
+      })
 
     } else if (event.target.textContent === 'economy'){
       console.log('economy');
       event.target.classList.add('is-active');
-      newsTabs[0].classList.remove('is-active');
-      newsTabs[2].classList.remove('is-active');
+      tabsItems[0].classList.remove('is-active');
+      tabsItems[2].classList.remove('is-active');
+      console.log(allData[1])
+      const newsList = document.querySelector('.news_Lists');
+
+      const imgWrapper = document.querySelector('.news_Img');
+      imgWrapper.innerHTML = "";
+      const img = document.createElement('img');
+      img.src = allData[1].image.img;
+      img.alt = allData[1].image.alt;
+      imgWrapper.appendChild(img);
+      console.log(img);
+
+      newsList.innerHTML = "";
+      Object.keys(allData[1].article).forEach((key)=>{
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const comment = document.createElement('span');
+        a.href = allData[1].article[key].url;
+        a.textContent = allData[1].article[key].title;
+        comment.textContent = allData[1].article[key].commentCount;
+        li.appendChild(a);
+        li.appendChild(comment);
+        lists.appendChild(li);
+        if(allData[1].article[key].is_new){
+          console.log('new article');
+        }
+      })
+
     } else if (event.target.textContent === 'sports'){
       console.log('sports');
       event.target.classList.add('is-active');
-      newsTabs[0].classList.remove('is-active');
-      newsTabs[1].classList.remove('is-active');
+      tabsItems[0].classList.remove('is-active');
+      tabsItems[1].classList.remove('is-active');
+      console.log(allData[2])
+      const newsList = document.querySelector('.news_Lists');
+
+      const imgWrapper = document.querySelector('.news_Img');
+      imgWrapper.innerHTML = "";
+      const img = document.createElement('img');
+      img.src = allData[2].image.img;
+      img.alt = allData[2].image.alt;
+      imgWrapper.appendChild(img);
+      console.log(img);
+
+      newsList.innerHTML = "";
+      Object.keys(allData[2].article).forEach((key)=>{
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        const comment = document.createElement('span');
+        a.href = allData[2].article[key].url;
+        a.textContent = allData[2].article[key].title;
+        comment.textContent = allData[2].article[key].commentCount;
+        li.appendChild(a);
+        li.appendChild(comment);
+        lists.appendChild(li);
+        if(allData[2].article[key].is_new){
+          console.log('new article');
+        }
+      })
     }
   });
 }
@@ -124,7 +203,8 @@ const showModal = () => {
 const init = async (inputNum,inputName) => {
   // createLoadingImage();
   const response = await getData();
-  const allData = await response.json();
+  allData = await response.json();
+  console.log(allData);
   createNewsTabs(allData.data);
   createNewsContent(allData.data);
 }
